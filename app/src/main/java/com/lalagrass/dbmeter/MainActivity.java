b03224a1b17659ad8dbef338727a28ecefb7a217
Yu-Ying <aaflying@msn.com>
2015-10-31 01:42:46 +0800

    fix db value

--- app/src/main/java/com/lalagrass/dbmeter/MainActivity.java
+++ app/src/main/java/com/lalagrass/dbmeter/MainActivity.java
@@ -4,12 +4,13 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int minimum = 1;
     private static volatile boolean isRecording = false;
     private static final int frequency = 44100;
     private static final int channelConfiguration = AudioFormat.CHANNEL_IN_MONO;
@@ -43,36 +44,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class RecordAudio extends AsyncTask<Void, Double, Void> {
+        private int avgCount = 1;
+        private double avg = 0;
+        private int count = 0;
         @Override
         protected Void doInBackground(Void... params) {
             try {
+
                 int bufferSize = AudioRecord.getMinBufferSize(frequency,
                         channelConfiguration, audioEncoding);
-                if (bufferSize < frequency)
-                    bufferSize = frequency;
+                avgCount = frequency / bufferSize;
+                if (avgCount < 1)
+                    avgCount = 1;
                 AudioRecord audioRecord = new AudioRecord(
                         MediaRecorder.AudioSource.MIC, frequency,
                         channelConfiguration, audioEncoding, bufferSize);
-                byte[] buffer = new byte[bufferSize];
+                short[] buffer = new short[bufferSize];
                 int offset = 0;
                 audioRecord.startRecording();
                 while (isRecording) {
                     while (offset < bufferSize) {
                         int bufferReadResult = audioRecord.read(buffer, offset,
                                 bufferSize - offset);
-                        offset+= bufferReadResult;
+                        offset += bufferReadResult;
                     }
 
-
-                        long sum = 0;
-                        for (int i = 0; i < bufferSize; i += 2) {
-                            sum += Math.pow((buffer[i] & 0xff) << 8 | buffer[i + 1], 2);
-                        }
-                        double amplitude = (double) (sum / (bufferSize / 2));
-                        double amplitudeDb = 20 * Math
-                                .log10(Math.abs(amplitude) / 32768);
-                        offset = 0;
-                        publishProgress(amplitudeDb);
+                    double sum = 0;
+                    for (int i = 0; i < bufferSize; i++) {
+                        sum += Math.abs(buffer[i]);
+                    }
+                    double amplitude = sum / bufferSize;
+                    double amplitudeDb = 20 * Math
+                            .log10(amplitude / minimum);
+                    offset = 0;
+                    publishProgress(amplitudeDb);
                 }
                 audioRecord.stop();
             } catch (Exception ex) {
@@ -80,14 +85,17 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+
         protected void onProgressUpdate(Double... progress) {
-            double value = progress[0];
-            if (value < 0) {
-                statusText.setText("0.0");
-            } else {
+            avg += progress[0];
+            if (++count >= avgCount) {
+                double value = avg / avgCount;
+                avg = 0;
+                count = 0;
                 statusText.setText(String.format("%.01f", value));
             }
         }
+
         protected void onPostExecute(Void result) {
         }
     }
